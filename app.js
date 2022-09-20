a74a2f5c7af295e46d78975ae9d2bbe8fd4efdc1
Mamoor khan <khanmamoor007@gmail.com>
2022-09-20 22:09:35 +0530

    New

---
+++ app.js
@@ -0,0 +1,28 @@
+(function () {
+  let screen = document.querySelector(".screen");
+  let buttons = document.querySelectorAll(".btn");
+  let equal = document.querySelector(".btn-equal");
+  let clear = document.querySelector(".btn-clear");
+
+  buttons.forEach(function (button) {
+    button.addEventListener("click", function (e) {
+      let value = e.target.dataset.num;
+      console.log(value);
+      screen.value += value;
+    });
+  });
+
+  equal.addEventListener("click", function () {
+    if (screen.value === "") {
+      screen.value = "please enter";
+    } else {
+      let answer = eval(screen.value);
+      console.log(answer);
+      screen.value = answer;
+    }
+  });
+
+  clear.addEventListener("click", function (e) {
+    screen.value = "";
+  });
+})();
